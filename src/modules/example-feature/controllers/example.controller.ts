cd5bbd6c340ab7e6b6943e87bb1ce6f63074a219
ostheperson <seyiojo13@gmail.com>
2023-07-11 21:00:26 +0100

    fix: logger

--- src/modules/example-feature/controllers/example.controller.ts
+++ src/modules/example-feature/controllers/example.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { ExampleService } from "../services/example.service";
+import { logger } from "../../../shared/logger";
 
 export class ExampleController {
   private exampleService: ExampleService;
@@ -8,6 +9,7 @@ export class ExampleController {
   }
   getExample(req: Request, res: Response) {
     const result = this.exampleService.getExample();
+    logger.info({ result });
     return res.json({ message: "Successful", data: result });
   }
 }
