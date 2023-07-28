d750f7f61c21438688426199e58c87371c0fa3e4
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-07-28 02:46:40 +0100

    email working

--- src/shared/utils/emails/templates/index.ts
+++ src/shared/utils/emails/templates/index.ts
@@ -15,18 +15,28 @@ function sendLoginLinkEmail({ user, linkId }: Options) {
 
 	const link = `${config.frontendUrl}/?id=${linkId}`;
 
-	const text = `
-            Hi ${firstName},
-            
-            Use the link below to log into your account.
-
-            <a href="${link}">${link}</a>
-        
-            Please hurry, the link will expired shortly.
-        
-            Regards,
-            Lawgecko Team
+	const html = `
+          <html>
+               <head>
+                    <title>Login Link Email</title>
+               </head>
+               <body style="font-family: Arial, sans-serif; line-height: 1.6; max-width: 600px; margin: 0 auto;">
+                    <p style="font-size: 18px; margin-bottom: 10px;">Hi ${firstName},</p>
+                    <p style="font-size: 16px; margin-bottom: 10px;">Use the link below to log into your account:</p>
+                    <p style="font-size: 16px; margin-bottom: 20px;">
+                    <a href="${link}" style="color: #007bff; text-decoration: none;"><button>Verify Email</button></a>
+                    </p>
+                    <p style="font-size: 16px; margin-bottom: 20px;">Please hurry, the link will expire shortly.</p>
+                    <p style="font-size: 16px;">Regards,</p>
+                    <p style="font-size: 16px;">Lawgecko Team</p>
+               </body>
+          </html>
        `;
 
-	return sendMail({ from: 'support', to: email, subject: 'Login link', text });
+	return sendMail({
+		from: 'ourlawgecko@gmail.com',
+		to: email,
+		subject: 'Login link',
+		html,
+	});
 }
